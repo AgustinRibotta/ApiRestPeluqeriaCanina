b18f25cb29a40e9a91d7342c62dbcf8aaaa2108b
AgustinRibotta <agustin.ribotta.r@gmail.com>
2024-12-15 20:32:36 +0100

    Inicio del proyecto

---
+++ src/main/java/com/PeluCanina/PeluqueriaCaniana/PeluqueriaCanianaApplication.java
@@ -0,0 +1,13 @@
+package com.PeluCanina.PeluqueriaCaniana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeluqueriaCanianaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeluqueriaCanianaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PeluCanina/PeluqueriaCaniana/PeluqueriaCanianaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PeluCanina.PeluqueriaCaniana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeluqueriaCanianaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
